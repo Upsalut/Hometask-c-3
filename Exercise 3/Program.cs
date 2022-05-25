557584f9dd246921fc6c7b87e660b06b4b7fae8f
Upper <goodp@yandex.ru>
2022-05-25 20:19:16 +0300

    ex 3

---
+++ Exercise 3/Program.cs
@@ -0,0 +1,11 @@
+﻿//Найти сумму чисел от 1 до А
+Console.WriteLine("Enter the number: ");
+int number=int.Parse(Console.ReadLine());
+int count =0;
+int digit = 0;
+while (count<=number)
+{
+    digit=digit+count;
+    count++;
+}
+Console.WriteLine(digit);
