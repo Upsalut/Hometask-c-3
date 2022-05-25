45116878fbe5ceb1375b9b7339c188660d913f85
Upper <goodp@yandex.ru>
2022-05-25 20:51:26 +0300

    ex 4

---
+++ Exercise 4/Program.cs
@@ -0,0 +1,10 @@
+﻿// Подсчитать сумму цифр в числе
+Console.WriteLine("Enter a number from:");
+int number = int.Parse(Console.ReadLine());
+int count = 0;
+while (number>0)
+{
+    number=number/10;
+    count++;
+}
+Console.WriteLine(count);
