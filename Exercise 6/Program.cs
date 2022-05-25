3cde03c51aa144c2aceea3a2844f76531f4e5d86
Upper <goodp@yandex.ru>
2022-05-25 22:26:49 +0300

    ex 6

---
+++ Exercise 6/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+int[] array= new int[8];
+for (int i=1; i<=8; i++)
+{
+    array[i-1]=new Random().Next(0,2);
+}
+Console.WriteLine();
+printArray(array);
+void printArray(int[] array)
+{
+    for (int i=0; i<array.Length;i++)
+  {
+    Console.Write(array[i] + " ");
+  } 
+}  
