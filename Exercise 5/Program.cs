602da6f229781824a2aaf8238785c67c5a313d26
Upper <goodp@yandex.ru>
2022-05-25 21:52:12 +0300

    ex 5

---
+++ Exercise 5/Program.cs
@@ -0,0 +1,14 @@
+﻿//Показать кубы чисел, заканчивающихся на четную цифру
+Console.WriteLine("Enter the number: ");
+int number=int.Parse(Console.ReadLine());
+int count =0;
+while (count<number)
+{
+    int digit = count +1;
+    int digitCubed = digit*digit*digit;
+    if (digitCubed%2==0)
+    {
+Console.WriteLine(digitCubed+ " ");
+    }
+    count++;
+}
